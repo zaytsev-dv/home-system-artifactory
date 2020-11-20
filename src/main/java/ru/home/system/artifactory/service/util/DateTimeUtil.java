4d8e4405ee83c2e02cd5f03cf7be4854c4f86ed2
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-11-20 17:15:04 +0300

    add strToDate

--- src/main/java/ru/home/system/artifactory/service/util/DateTimeUtil.java
+++ src/main/java/ru/home/system/artifactory/service/util/DateTimeUtil.java
@@ -1,8 +1,15 @@
 package ru.home.system.artifactory.service.util;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
+@Slf4j
 public class DateTimeUtil
 {
 	public static final String YYYY_MM_DD_T_HH_MM = "yyyy-MM-dd'T'HH:mm";
@@ -21,5 +28,21 @@ public class DateTimeUtil
 	{
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
 		return LocalDateTime.parse(str, formatter);
+	}
+
+	public static Date strToDate(String date, String pattern)
+	{
+		DateFormat format = new SimpleDateFormat(pattern);
+		Date result = null;
+		try
+		{
+			result = format.parse(date);
+		}
+		catch (ParseException e)
+		{
+			log.error(e.getMessage(), e);
+		}
+
+		return result;
 	}
 }
